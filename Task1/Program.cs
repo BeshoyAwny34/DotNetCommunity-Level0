4dc13b35b39b40a8da61221926b80c0e2c3f1536
Eng-BeshoyAwny <BeshoyAwny34@gmail.com>
2026-07-07 23:38:38 +0300

    Answer Question 1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Write a C# Sharp program to print Hello and your name in a separate line.
+        Console.WriteLine("Hello: Beshoy Awny");
+    }
+}
