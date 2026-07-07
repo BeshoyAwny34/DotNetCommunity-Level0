e0d655085bcff5356d168e324e8ca66c52eab0aa
Eng-BeshoyAwny <BeshoyAwny34@gmail.com>
2026-07-07 23:42:49 +0300

    Answer Question 3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Write a C# Sharp program to print the result of dividing two numbers.
+        float num1 = 50, num2 = 100 ,Divide;
+        Divide = num1 / num2;
+        Console.WriteLine($"{num1} / {num2} = {Divide}");
+        Console.ReadKey();
+    }
+}
