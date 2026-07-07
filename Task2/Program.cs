6ed7cdc6cab32f51e3f6a26af829dc11fd3cf5e6
Eng-BeshoyAwny <BeshoyAwny34@gmail.com>
2026-07-07 23:42:04 +0300

    Answer Question 2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Write a C# Sharp program to print the sum of two numbers.
+        int num1 = 50,num2=100,Sum;
+        Sum = num1 + num2;
+        Console.WriteLine($"{num1} + {num2} = {Sum}");
+        Console.ReadKey();
+    }
+}
