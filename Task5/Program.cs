63965a1a92e9c8a63ffabef1f8992af42699b2c6
Eng-BeshoyAwny <BeshoyAwny34@gmail.com>
2026-07-07 23:57:00 +0300

    Answer Question 5

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task5;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int FirstNumber = 5, SecondNumber = 6,SwapNumber;
+        SwapNumber = FirstNumber;
+        FirstNumber = SecondNumber;
+        SecondNumber = SwapNumber;
+        Console.WriteLine($"First Number : {FirstNumber}");
+        Console.WriteLine($"Second Number : {SecondNumber}");
+
+    }
+}
